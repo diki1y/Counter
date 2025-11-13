dc79290b2d58e4e79b1f701a2798f2ea901d7660
diki1y <le9edev1eo@yandex.ru>
2025-11-13 11:23:59 +0300

    Выполнил задачу спринта 3

--- Counter/ViewController.swift
+++ Counter/ViewController.swift
@@ -8,12 +8,50 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    private var counter = 0
+    
+    
+    @IBOutlet weak var counterLabel: UILabel!
+    @IBOutlet weak var historyTextView: UITextView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        counterLabel.text = "0"
+        historyTextView.text = "История изменений:\n"
+        
     }
-
-
+    
+    @IBAction func plusButton(_ sender: Any) {
+        counter += 1
+        updateUI(change: "+1")
+    }
+    
+    @IBAction func resetButton(_ sender: Any) {
+        counter = 0
+        updateUI(change: "значение сброшено")
+    }
+    
+    @IBAction func minusButton(_ sender: Any) {
+        if counter > 0 {
+            counter -= 1
+            updateUI(change: "-1")
+        } else {
+            appendToHistory(text: "попытка уменьшить значение счётчика ниже 0")
+        }
+    }
+    private func updateUI(change: String) {
+        counterLabel.text = "\(counter)"
+        appendToHistory(text: "значение изменено на \(change)")
+    }
+    private func appendToHistory(text: String) {
+        let dateFormatter = DateFormatter()
+        dateFormatter.dateFormat = "HH:mm:ss"
+        let timestamp = dateFormatter.string(from: Date())
+        historyTextView.text += "[\(timestamp)]: \(text)\n"
+        
+        let bottom = NSRange(location: historyTextView.text.count - 1, length: 1)
+        historyTextView.scrollRangeToVisible(bottom)
+    }
 }
-
