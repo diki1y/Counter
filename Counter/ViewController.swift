68424ac99ac11528e59979812f52f7c631d6356b
diki1y <le9edev1eo@yandex.ru>
2025-11-17 15:49:48 +0300

    Новые изменения в проекте

--- Counter/ViewController.swift
+++ Counter/ViewController.swift
@@ -11,47 +11,56 @@ class ViewController: UIViewController {
     
     private var counter = 0
     
-    
-    @IBOutlet weak var counterLabel: UILabel!
-    @IBOutlet weak var historyTextView: UITextView!
+    @IBOutlet private weak var counterLabel: UILabel!
+    @IBOutlet private weak var historyTextView: UITextView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
         counterLabel.text = "0"
         historyTextView.text = "История изменений:\n"
-        
     }
     
-    @IBAction func plusButton(_ sender: Any) {
+    private func updateUI(change: String) {
+        
+        counterLabel.text = "\(counter)"
+        
+        if change == "сброшено" {
+            appendToHistory(text: "значение сброшено")
+        } else {
+            appendToHistory(text: "значение изменено на \(change)")
+        }
+    }
+    
+    
+    private func appendToHistory(text: String) {
+        let dateFormatter = DateFormatter()
+        dateFormatter.dateFormat = "HH:mm:ss"
+        
+        let timestamp = dateFormatter.string(from: Date())
+        historyTextView.text += "[\(timestamp)]: \(text)\n"
+        
+        let bottom = NSRange(location: historyTextView.text.count - 1, length: 1)
+        historyTextView.scrollRangeToVisible(bottom)
+    }
+    
+    
+    @IBAction private func plusButton(_ sender: Any) {
         counter += 1
         updateUI(change: "+1")
     }
     
-    @IBAction func resetButton(_ sender: Any) {
+    @IBAction private func resetButton(_ sender: Any) {
         counter = 0
-        updateUI(change: "значение сброшено")
+        updateUI(change: "сброшено")
     }
     
-    @IBAction func minusButton(_ sender: Any) {
+    @IBAction private func minusButton(_ sender: Any) {
         if counter > 0 {
             counter -= 1
             updateUI(change: "-1")
         } else {
             appendToHistory(text: "попытка уменьшить значение счётчика ниже 0")
         }
-    }
-    private func updateUI(change: String) {
-        counterLabel.text = "\(counter)"
-        appendToHistory(text: "значение изменено на \(change)")
-    }
-    private func appendToHistory(text: String) {
-        let dateFormatter = DateFormatter()
-        dateFormatter.dateFormat = "HH:mm:ss"
-        let timestamp = dateFormatter.string(from: Date())
-        historyTextView.text += "[\(timestamp)]: \(text)\n"
-        
-        let bottom = NSRange(location: historyTextView.text.count - 1, length: 1)
-        historyTextView.scrollRangeToVisible(bottom)
     }
 }
